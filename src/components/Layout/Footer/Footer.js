80e87f17632434e3f9f95dff91bb9c850bad53d8
Jonathan Leack <jonathan.leack@pwc.com>
2019-08-04 17:51:04 -0700

    Linter 2.

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Footer.css';
+import classes from "./Footer.css";
 
 /*======================================================================
 // This is the footer for the app.
 ======================================================================*/
-const Header = (props) => (
+const Header = props => (
   <footer className={classes.Footer}>
-    <p>Made by <a href="http://www.jonathanleack.com/" target="_blank" rel="noopener noreferrer" alt="Jonathan Leack portfolio">Jonathan Leack</a>.</p>
+    <p>
+      Made by{" "}
+      <a
+        href="http://www.jonathanleack.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="Jonathan Leack portfolio"
+      >
+        Jonathan Leack
+      </a>
+      .
+    </p>
   </footer>
-)
+);
 
 export default Header;
